bbbaa8d64baf37a7f568d89c2005d9dc19dfffcf
Andres Rodriguez <andres6459@gmail.com>
2021-05-20 13:04:42 -0700

    Moved data to App component to be global.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,31 +1,9 @@
 import React from 'react'
 
-const tasks = [
-    {
-		id: 1,
-		text: 'Doctors Appointment',
-		day: 'Feb 5th at 2:30pm',
-		reminder: true,
-    },
-	{
-		id: 2,
-		text: 'Meeting at School',
-		day: 'Feb 6th at 1:30pm',
-		reminder: true,
-	},
-	{
-		id: 3,
-		text: 'Food Shopping',
-		day: 'Feb 7th at 2:30pm',
-		reminder: false,
-	}
-];
-
-const Tasks = () => {
-    return (
+const Tasks = (props) => {
+	return (
         <div>
-            {tasks.map((task) => (<h3>{task.text}</h3>))}
-			<h2>Hello</h2>
+            {props.tasks.map((task) => (<h3>{task.text}</h3>))}
         </div>
     )
 }
