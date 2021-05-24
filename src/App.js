025769b900eb81737e560761f3fa2b4847b0d1db
Andres Rodriguez <andres6459@gmail.com>
2021-05-24 12:39:10 -0700

    Changing task reminder in UI updated the task reminder in json server with PUT request.

--- src/App.js
+++ src/App.js
@@ -27,11 +27,32 @@ const App = () => {
 		return data
 	}
 
+	// Fetch Task
+	const fetchTask = async (id) => {
+		const result = await fetch(`http://localhost:5000/tasks/${id}`)
+		const data = await result.json()
+
+		return data
+	}
+
 	// Add new Task
-	const addTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1
-		const newTask = {id, ...task}
-		setTasks([...tasks, newTask])
+	const addTask = async (task) => {
+		const result = await fetch('http://localhost:5000/tasks',
+			{
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json', 
+				},
+				body: JSON.stringify(task),
+			})
+		
+		const data = await result.json()
+
+		setTasks([...tasks, data])
+
+		// const id = Math.floor(Math.random() * 10000) + 1
+		// const newTask = {id, ...task}
+		// setTasks([...tasks, newTask])
 	}
 
 	// Delete Task
@@ -42,11 +63,25 @@ const App = () => {
 	}
 
 	// Toggle the boolean reminder
-	const toggleReminder = (id) => {
+	const toggleReminder = async (id) => {
+		const taskToToggle = await fetchTask(id)
+		const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+		const result = await fetch(`http://localhost:5000/tasks/${id}`,
+			{
+				method: 'PUT',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify(updatedTask)
+			})
+
+			const data = await result.json()
+
+			setTasks(...tasks, data)
+
 		setTasks(
 			tasks.map((task) => 
 				task.id === id ? {...task, reminder: 
-					!task.reminder } : task)
+					data.reminder } : task)
 		)
 	}
 
